7382d1bbce9fd7b0b59640b80527813b627aa36c
Vinindu Wickramasinghe[IT/HO/BRW] <vininduw@brownsgroup.com>
2025-05-05 09:09:06 +0530

    Day 02

--- Start/1_PythonFeatures/specialnames.py
+++ Start/1_PythonFeatures/specialnames.py
@@ -1,12 +1,17 @@
 # Example file for Advanced Python by Joe Marini
 # Using special module names
 
-
+import collections
 # __name__ is the name of the module
 print("Module Name:", __name__)
+print(collections.__name__)
 
 # __file__ contains the path to the file from which the module was loaded
 print("File Path:", __file__)
 
 # __package__ indicates the package that the module belongs to.
 print("Module Package:", __package__)
+print(collections.__package__)
+
+if __name__ == "__main__":
+  print("This code is being run directly.")
